2b46494611172813b8fba95cdb036faf33b68566
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2025-05-09 11:14:25 -0700

    added the properties and methods for vechile class

---
+++ src/main/java/Dealership.java
@@ -0,0 +1,68 @@
+public class Dealership {
+    String name;
+    String address;
+    String phone;
+
+    Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getVehicleByPrice() {
+
+        return "";
+    }
+
+    ;
+
+    public String getVehicleByMakeModel() {
+
+        return "";
+    }
+
+    ;
+
+    public String getVehicleByColor() {
+
+        return "";
+    }
+
+    ;
+
+    public String getVehicleByYear() {
+
+        return "";
+    }
+
+    ;
+
+    public String getVehicleByMileage() {
+
+        return "";
+    }
+
+    ;
+
+    public String getVehicleByType() {
+
+        return "";
+    }
+
+    ;
+
+    public String getAllVehicles() {
+
+        return "";
+    }
+
+    ;
+
+    public String removeVehicles() {
+
+        return "";
+    }
+
+    ;
+
+}
