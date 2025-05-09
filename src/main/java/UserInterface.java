97e844b889d1485577699595f62f56deaf60d3cf
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2025-05-09 11:40:37 -0700

    created the main and Userinterface classes

---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,18 @@
+public class UserInterface {
+
+
+    public void display(){
+
+    }
+    public void init(){
+
+    }
+    public void displayVehicles(){
+
+    }
+    public void processAllVehiclesRequest(){
+
+    }
+
+
+}
